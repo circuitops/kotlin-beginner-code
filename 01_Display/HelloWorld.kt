5c5bbafdec280194b629dd3a164b909f0f615d96
circuitops <59457328+circuitops@users.noreply.github.com>
2020-01-08 11:47:03 +0800

    Add files via upload

---
+++ 01_Display/HelloWorld.kt
@@ -0,0 +1,10 @@
+package `01_Display`
+
+fun main() {                                    //Main function - main execution point of Kotlin
+    print("Hello World!!!")                     //print("") - Display Hello World!!!
+    print("Hello Again!!!")                     //print("") - Display Hello Again!!!
+
+    println("Rejection is Great")               //println("") - Display Rejection is Great and a new line
+    println("So don't fall in Depression")      //println("") - Display So don't fall in Depression and a new line
+}
+
