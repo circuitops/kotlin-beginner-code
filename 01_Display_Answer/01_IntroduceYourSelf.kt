244c2a58e20a9f85df9b7f73744282fd8e0c8a86
circuitops <59457328+circuitops@users.noreply.github.com>
2020-01-08 13:05:14 +0800

    Delete 01_IntroduceYourSelf.kt

--- 01_Display_Answer/01_IntroduceYourSelf.kt
+++
@@ -1,9 +0,0 @@
-package `01_Display_Answer`
-
-fun main(){
-    println("Name: Erik Nunn")
-    println("Address: Seattle, USA")
-    println("Birth Date: January 1, 1980")
-    println("Age: 40")
-    println("Hubby: Anime, Programming")
-}
