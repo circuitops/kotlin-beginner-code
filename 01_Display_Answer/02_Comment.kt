3bcbf9117133ca2ede52624507c777b6f08a9bf9
circuitops <59457328+circuitops@users.noreply.github.com>
2020-01-08 13:58:01 +0800

    Add files via upload

---
+++ 01_Display_Answer/02_Comment.kt
@@ -0,0 +1,13 @@
+package `01_Display_Answer`
+
+fun main(){
+    println("Name: Erik Nunn")
+    println("Address: Seattle, USA")
+    println("Birth Date: January 1, 1980")
+    println("Age: 40")
+    println("Likes: Anime")
+    println("Dislikes: Dogs")
+
+    // Likes: i like Anime, because Anime brings be happiness and laughter everyday.
+    //Dislikes: i hate dogs. I have allergy
+}
