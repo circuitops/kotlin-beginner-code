c86b36ce4b7262d66e5e20a218646f9e77726aec
circuitops <59457328+circuitops@users.noreply.github.com>
2020-01-14 20:48:20 +0800

    Add files via upload

---
+++ 10_EqualityAndInequalityOperators/01_IsEqualTo.kt
@@ -0,0 +1,9 @@
+package `10_EqualityAndInequalityOperators`
+
+fun main(){
+    val a = 120
+    val b = 20
+
+    val result = a == b     //Result is False, 120(a) is equal to 20(b) is not true
+    println(result)
+}
---
+++ 10_EqualityAndInequalityOperators/03_NotEqualTo.kt
@@ -0,0 +1,9 @@
+package `10_EqualityAndInequalityOperators`
+
+fun main(){
+    val a = 120
+    val b = 20
+
+    val result = a != b     //Result is True, 120(a) is not equal to 20(b) is true
+    println(result)
+}
