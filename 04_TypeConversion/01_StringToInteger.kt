222798ce4a69d6d3479cfe6a28bab0e5bd460a90
circuitops <59457328+circuitops@users.noreply.github.com>
2020-01-14 14:46:46 +0800

    Add files via upload

---
+++ 04_TypeConversion/01_StringToInteger.kt
@@ -0,0 +1,8 @@
+package `04_TypeConversion`
+
+fun main(){
+    val myAgeString = "20"                  //String
+
+    val myAgeInteger = myAgeString.toInt()  //Convert String to Integer
+    println(myAgeInteger)
+}
---
+++ 04_TypeConversion/02_IntegerToDouble.kt
@@ -0,0 +1,7 @@
+package `04_TypeConversion`
+
+fun main() {
+    val myMoneyInteger = 2000                       //Integer
+    val myMoneyDouble = myMoneyInteger.toDouble()   //Convert Integer to Double
+    println("My Current Money: $myMoneyDouble")     //Print
+}
